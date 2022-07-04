22e27fa836dd9c467ad2d383ab6cdef0fa38027d
mrbrightsiide <devkimsia@gmail.com>
2022-07-04 23:44:26 +0900

    Add a new action 'CLEAR' for clear the order form

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,12 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
+  return defaultCartState;
 };
 
 const CartProvider = (props) => {
@@ -72,11 +78,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
